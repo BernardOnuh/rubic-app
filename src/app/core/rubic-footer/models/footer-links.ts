0210c579f2b9dcb43bb95df012df17c4aa6e3f26
Bernard Onuh <onuhbernard4@gmail.com>
2023-12-07 12:38:39 +0100

    commit

--- src/app/core/rubic-footer/models/footer-links.ts
+++ src/app/core/rubic-footer/models/footer-links.ts
@@ -2,7 +2,7 @@ import { FooterLink } from 'src/app/core/rubic-footer/models/footer-link';
 
 export const FOOTER_LINKS: FooterLink[] = [
   {
-    dark: 'assets/images/contacts/twitter.svg',
+    dark: 'assets/images/contacts/xtwitter.png',
     light: 'assets/images/contacts/twitter_light.svg',
     altText: 'Twitter',
     link: 'https://twitter.com/fetsedgetech'
